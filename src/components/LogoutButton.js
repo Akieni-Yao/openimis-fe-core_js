e0d086d5248a0ab416c4dcf920ae7eaf3726f45a
ankitkumar774 <135097567+ankitkumar774@users.noreply.github.com>
2024-03-06 12:00:31 +0530

    correction in logout

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 import { IconButton } from "@material-ui/core";
 import { ExitToApp } from "@material-ui/icons";
 import { CheckAssignedProfile, logout } from "../actions";
